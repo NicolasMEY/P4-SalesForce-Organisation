80853bb672c0bcec818d808b7eb79d61e207f103
nicolasmeyer <nsg.meyer@hotmail.fr>
2025-03-22 18:18:32 +0100

    🎨 opportunityProductList.js : Create base js structure for component

--- force-app/main/default/lwc/opportunityProductList/opportunityProductList.js
+++ force-app/main/default/lwc/opportunityProductList/opportunityProductList.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class OpportunityProductList extends LightningElement {}
+export default class OpportunityProductList extends LightningElement {
+  @track products = []; // Liste des produits
+  @track columns = [
+    { label: "Nom du produit", fieldName: "name" },
+    { label: "Quantité", fieldName: "quantity" },
+    { label: "Prix unitaire", fieldName: "unitPrice" },
+    { label: "Prix Total", fieldName: "totalPrice" },
+    { label: "Quantité en Stock", fieldName: "stockQuantity" },
+    {
+      label: "Supprimer",
+      type: "button",
+      typeAttributes: {
+        label: "Supprimer",
+        name: "delete",
+        variant: "destructive"
+      }
+    },
+    {
+      label: "Voir produit",
+      type: "button",
+      typeAttributes: {
+        label: "Voir produit",
+        name: "view",
+        variant: "neutral"
+      }
+    }
+  ];
+}
