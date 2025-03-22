52cf0aae9a6ee59b012dfacbdddb557741520a80
nicolasmeyer <nsg.meyer@hotmail.fr>
2025-03-22 19:01:39 +0100

    ✨ opportunityProductList.js : Fetch opportunity products data with @wire

--- force-app/main/default/lwc/opportunityProductList/opportunityProductList.js
+++ force-app/main/default/lwc/opportunityProductList/opportunityProductList.js
@@ -1,9 +1,12 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, api, wire } from "lwc";
+import getOpportunityProducts from "@salesforce/apex/OpportunityProductController.getOpportunityProducts";
 
 export default class OpportunityProductList extends LightningElement {
+  @api recordId; // ID de l'opportunité
+  error; // Pour stocker l'erreur, s'il y en a
   @track products = []; // Liste des produits
   @track columns = [
-    { label: "Nom du produit", fieldName: "name" },
+    { label: "Nom du produit", fieldName: "productName" },
     { label: "Quantité", fieldName: "quantity" },
     { label: "Prix unitaire", fieldName: "unitPrice" },
     { label: "Prix Total", fieldName: "totalPrice" },
@@ -27,4 +30,21 @@ export default class OpportunityProductList extends LightningElement {
       }
     }
   ];
+
+  // Il manque la gestion des buttons delete et voir produit
+
+  // il manque la récupération des datas au niveau de la class APEX
+
+  @wire(getOpportunityProducts, { opportunityId: "$recordId" })
+  wiredProducts({ data, error }) {
+    if (data) {
+      // Si la donnée est récupérée, on la stocke dans 'products'
+      this.products = data;
+      this.error = undefined; // Réinitialiser l'erreur si la récupération est réussie
+    } else if (error) {
+      // En cas d'erreur, afficher l'erreur
+      this.error = error;
+      this.products = [];
+    }
+  }
 }
